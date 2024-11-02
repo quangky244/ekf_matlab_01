d01a24851c62741068df446a0490249299ad40e8
KYSSPECTRE\kytran <quangky244@gmail.com>
2024-11-02 22:27:10 +0100

    Modified playground1 to run

--- playGround1.m
+++ playGround1.m
@@ -1,38 +1,39 @@
+clear
 % Define the system parameters
 A = [0, 1; -.2, -.3];
-B = [0.5; 1];
+B = [0.05; 1];
 H = [1, 0];
-Q = [0.001, 0; 0, 0.0001];
-R = 0.005;
-P = eye(2);
-x = [0; 0];
+Q = [0.001, 0; 0, 0.001];
+R = 0.5;
+P = 10*eye(2);
+x = [10; 0];
 
 D = 0;
+
+syst = ss(A,B,H,D);
 
 T = 8e-3;
 tspan = 0:T:50;
 n_steps = length(tspan);
 reshape(tspan,length(tspan),1);
 uspan = ones(length(tspan),1);
-%uspan = 1*sin(tspan*2*pi*1);
+% uspan = 1*sin(tspan*2*pi*1);
 [y,~,x_real] = lsim(syst,uspan,tspan);
 y = y + sqrt(R)*randn(n_steps,1);
 
-syst = ss(A,B,H,D);
-syst_d = c2d(syst,.5*T);
-
-
+syst_d = c2d(syst,T);
 figure(1);clf(1);
 step(syst); hold on;
 step(syst_d);
 
 % Create the Kalman Filter object
-% kf = KalmanFilter(syst_d.A, syst_d.B, syst_d.C, Q, R, P, x);
-kf = KalmanFilter(A, B, H, Q, R, P, x);
+kf = KalmanFilter(syst_d.A, syst_d.B, syst_d.C, Q, R, P, x);
+% kf = KalmanFilter(A, B, H, Q, R, P, x);
 u_hist = zeros(length(n_steps),1);
 y_est_hist = zeros(length(n_steps),1);
 x_hist = zeros(length(n_steps),2);
-for i = 1:length(tspan)
+x_hist(1,:) = x;
+for i = 2:length(tspan)
     % Predict and update steps
     u = uspan(i);  % Control input
     z = y(i);  % Measurement
