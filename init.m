07d84aaa99709d1fdb651e9f302afa23f2de9037
KYSSPECTRE\kytran <quangky244@gmail.com>
2024-11-30 22:58:42 +0100

    added default line width into init

--- init.m
+++ init.m
@@ -2,3 +2,4 @@
 set(0,'DefaultFigureWindowStyle','docked'); % set default docking mode.
 set(groot,'defaultAxesXGrid','on');
 set(groot,'defaultAxesYGrid','on');
+set(0, 'DefaultLineLineWidth', 1.25);
